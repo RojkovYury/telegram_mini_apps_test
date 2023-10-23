9e05e73788a72154c791ebea81c3d8e2bedfe13e
RojkovYury <rojkov.yury@gmail.com>
2023-10-23 23:36:43 +0300

    3

--- src/App.js
+++ src/App.js
@@ -78,8 +78,8 @@ function App() {
     <div className="App">
         <Box sx={{ display: 'flex', flexDirection: 'column', px:3, pt: 5 }}>
 
-          <div style={{ width: '100%', paddingBottom: '16px', marginBottom: '2px' }}>
-            <label style={{ paddingLeft: '10px' }}>Card Number</label>
+          <div style={{ width: '100%', paddingBottom: '16px' }}>
+            <label style={{ paddingLeft: '10px', marginBottom: '2px', fontSize: '16px' }}>Card Number</label>
             <div style={{ border: '2px solid black', borderRadius: '4px', display: 'flex' }}>
               <div style={{ width: '24px', height: '24px', marginTop: '10px', marginBottom: '10px', marginLeft: '10px', marginRight: '10px' }}><CreditCardIcon/></div>
               <input style={{ width: '100%', paddingTop: '10px', paddingBottom: '10px', paddingRight: '10px', border: 'none', outline: 'none', fontSize: '18px' }}/>
