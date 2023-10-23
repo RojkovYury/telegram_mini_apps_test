fe036ff5a4b8815229963183aef02637eb5d07a2
RojkovYury <rojkov.yury@gmail.com>
2023-10-23 07:05:10 +0300

    2

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ function App() {
 
   useEffect(() => { tg.ready(); })
 
-  // const onClose = () => { tg.close(); }
-
-  const [cardNumber, setCardNumber] = useState('cardNumber is 1234 5678 1234 5678');
+  const [cardNumber, setCardNumber] = useState('');
   const [nameOnCard, setNameOnCard] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
   const [cvv, setCvv] = useState('');
@@ -29,45 +27,156 @@ function App() {
     }); 
   })
 
-const onSendData = useCallback(()=>{
-  const data = { cardNumber }
-  tg.sendData(JSON.stringify(data))
-})
+  const onSendData = useCallback(()=>{
+    const data = { cardNumber }
+    tg.sendData(JSON.stringify(data))
+  })
 
-useEffect(() => { tg.onEvent('mainButtonClicked', onSendData); })
+  useEffect(() => { tg.onEvent('mainButtonClicked', onSendData); })
 
+
+  const tgTextFieldSx = { 
+    ml:1, 
+    mb: 2, 
+    input: { color: 'var(--tg-theme-text-color)' },
+    label: { color: 'var(--tg-theme-text-color)' },
+    div: { color: 'var(--tg-theme-text-color)' },
+  };
+
+  const handleCardNumberChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || /^[0-9\b]+$/.test(value)) {
+      setCardNumber(value);
+    }
+  };
+
+  const handleNameOnCardChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || /^[a-zA-Z\s]+$/.test(value)) {
+      setNameOnCard(value.toUpperCase());
+    }
+  };
+
+  const handleExpirationDateChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || /^[0-9/]+$/.test(value)) {
+      setExpiryDate(value);
+    }
+  };
+
+  const handleCvvChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || /^[0-9\b]+$/.test(value)) {
+      setCvv(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (cardNumber && expirationDate && cvv) {
+      console.log('Данные отправлены!');
+    } else {
+      console.log('Пожалуйста, заполните все поля');
+    }
+  };
 
   return (
     <div className="App">
-      <div>{userId}</div>
+         <form onSubmit={handleSubmit}>
 
-      <input id='CardNumber'></input>
-      <input id='NameOnCard'></input>
-      <input id='ExpirationDate'></input>
-      <input id='Cvv'></input>
+      <Typography>{WebAppUser}</Typography>
 
-      <button onClick={() => tg.BackButton.isVisible = true}>btn</button>
+      <Box sx={{ display: 'flex', flexDirection: 'column', px:3, pt: 5 }}>
+        <TextField
+          id="cardNumber"
+          label="Card Number:"
+          value={cardNumber}
+          onChange={handleCardNumberChange}
+          InputProps={{
+            endAdornment: null,
+            startAdornment: (
+              <InputAdornment position="start">
+                <CreditCardIcon />
+              </InputAdornment>
+            ),
+          }}
+          InputLabelProps={{ shrink: true }} 
+          focused
+          sx={ tgTextFieldSx } 
+        />
+
+        <TextField
+          id="nameOnCard"
+          label="Name on Card:"
+          value={nameOnCard} 
+          onChange={handleNameOnCardChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonIcon />
+              </InputAdornment>
+            ),
+          }}
+          InputLabelProps={{ shrink: true }} 
+          focused
+          sx={ tgTextFieldSx } 
+        />
+
+
+        <Box sx={{ display: 'flex' }}>
+          <TextField
+            id="expiryDate"
+            label="ExpiryDate:"
+            value={expiryDate}
+            onChange={handleExpirationDateChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <CalendarMonthIcon />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{ shrink: true }} 
+            focused
+            sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+          />
+
+          <TextField
+            id="cvv"
+            label="cvv:"
+            value={cvv}
+            onChange={handleCvvChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{ shrink: true }}
+            focused 
+            sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+          />
+        </Box>
+
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button variant='outlined' onClick={onClose} sx={{ ml:1, mb: 1, minWidth: '120px' }}>
+            Close
+          </Button>
+          <Button variant='contained' type="submit" sx={{ ml:1, mb: 1, minWidth: '120px' }}>
+            Submit
+          </Button>
+        </Box>
+
+      </Box>
+
+      </form>
+
     </div>
   );
 }
 
 export default App
-
-
-/*
-tg.initDataUnsafe.user.id // уникальный идентификатор пользователя
-tg.initDataUnsafe.user.isBot // бот ли пользователь (true/false)
-tg.initDataUnsafe.user.first_name // имя пользователя
-tg.initDataUnsafe.user.last_name // "фамилия" пользователя
-tg.initDataUnsafe.user.username // username пользователя
-tg.initDataUnsafe.user.language_code // код языка пользователя
-*/
-
-
-
-
-
-
 
 /*
 
@@ -120,93 +229,5 @@ tg.initDataUnsafe.user.language_code // код языка пользовател
 
 
 /*
-    <form onSubmit={handleSubmit}>
 
-        <Typography>{WebAppUser}</Typography>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column', px:3, pt: 5 }}>
-          <TextField
-            id="cardNumber"
-            label="Card Number:"
-            value={cardNumber}
-            onChange={handleCardNumberChange}
-            InputProps={{
-              endAdornment: null,
-              startAdornment: (
-                <InputAdornment position="start">
-                  <CreditCardIcon />
-                </InputAdornment>
-              ),
-            }}
-            InputLabelProps={{ shrink: true }} 
-            focused
-            sx={ tgTextFieldSx } 
-          />
-
-          <TextField
-            id="nameOnCard"
-            label="Name on Card:"
-            value={nameOnCard} 
-            onChange={handleNameOnCardChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonIcon />
-                </InputAdornment>
-              ),
-            }}
-            InputLabelProps={{ shrink: true }} 
-            focused
-            sx={ tgTextFieldSx } 
-          />
-       
-
-          <Box sx={{ display: 'flex' }}>
-            <TextField
-              id="expiryDate"
-              label="ExpiryDate:"
-              value={expiryDate}
-              onChange={handleExpirationDateChange}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <CalendarMonthIcon />
-                  </InputAdornment>
-                ),
-              }}
-              InputLabelProps={{ shrink: true }} 
-              focused
-              sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
-            />
-
-            <TextField
-              id="cvv"
-              label="cvv:"
-              value={cvv}
-              onChange={handleCvvChange}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockIcon />
-                  </InputAdornment>
-                ),
-              }}
-              InputLabelProps={{ shrink: true }}
-              focused 
-              sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
-            />
-          </Box>
-
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button variant='outlined' onClick={onClose} sx={{ ml:1, mb: 1, minWidth: '120px' }}>
-              Close
-            </Button>
-            <Button variant='contained' type="submit" sx={{ ml:1, mb: 1, minWidth: '120px' }}>
-              Submit
-            </Button>
-          </Box>
-
-        </Box>
-
-      </form>
 */
