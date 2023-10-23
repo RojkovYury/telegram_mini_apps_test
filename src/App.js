fe938c8483f04828730e4ca21198a770095085b0
RojkovYury <rojkov.yury@gmail.com>
2023-10-23 07:07:02 +0300

    2

--- src/App.js
+++ src/App.js
@@ -82,93 +82,91 @@ function App() {
 
   return (
     <div className="App">
-         <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
 
-      <Typography>{WebAppUser}</Typography>
-
-      <Box sx={{ display: 'flex', flexDirection: 'column', px:3, pt: 5 }}>
-        <TextField
-          id="cardNumber"
-          label="Card Number:"
-          value={cardNumber}
-          onChange={handleCardNumberChange}
-          InputProps={{
-            endAdornment: null,
-            startAdornment: (
-              <InputAdornment position="start">
-                <CreditCardIcon />
-              </InputAdornment>
-            ),
-          }}
-          InputLabelProps={{ shrink: true }} 
-          focused
-          sx={ tgTextFieldSx } 
-        />
-
-        <TextField
-          id="nameOnCard"
-          label="Name on Card:"
-          value={nameOnCard} 
-          onChange={handleNameOnCardChange}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PersonIcon />
-              </InputAdornment>
-            ),
-          }}
-          InputLabelProps={{ shrink: true }} 
-          focused
-          sx={ tgTextFieldSx } 
-        />
-
-
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', px:3, pt: 5 }}>
           <TextField
-            id="expiryDate"
-            label="ExpiryDate:"
-            value={expiryDate}
-            onChange={handleExpirationDateChange}
+            id="cardNumber"
+            label="Card Number:"
+            value={cardNumber}
+            onChange={handleCardNumberChange}
             InputProps={{
+              endAdornment: null,
               startAdornment: (
                 <InputAdornment position="start">
-                  <CalendarMonthIcon />
+                  <CreditCardIcon />
                 </InputAdornment>
               ),
             }}
             InputLabelProps={{ shrink: true }} 
             focused
-            sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+            sx={ tgTextFieldSx } 
           />
 
           <TextField
-            id="cvv"
-            label="cvv:"
-            value={cvv}
-            onChange={handleCvvChange}
+            id="nameOnCard"
+            label="Name on Card:"
+            value={nameOnCard} 
+            onChange={handleNameOnCardChange}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
-                  <LockIcon />
+                  <PersonIcon />
                 </InputAdornment>
               ),
             }}
-            InputLabelProps={{ shrink: true }}
-            focused 
-            sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+            InputLabelProps={{ shrink: true }} 
+            focused
+            sx={ tgTextFieldSx } 
           />
-        </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button variant='outlined' onClick={onClose} sx={{ ml:1, mb: 1, minWidth: '120px' }}>
-            Close
-          </Button>
-          <Button variant='contained' type="submit" sx={{ ml:1, mb: 1, minWidth: '120px' }}>
-            Submit
-          </Button>
-        </Box>
 
-      </Box>
+          <Box sx={{ display: 'flex' }}>
+            <TextField
+              id="expiryDate"
+              label="ExpiryDate:"
+              value={expiryDate}
+              onChange={handleExpirationDateChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <CalendarMonthIcon />
+                  </InputAdornment>
+                ),
+              }}
+              InputLabelProps={{ shrink: true }} 
+              focused
+              sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+            />
+
+            <TextField
+              id="cvv"
+              label="cvv:"
+              value={cvv}
+              onChange={handleCvvChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+              InputLabelProps={{ shrink: true }}
+              focused 
+              sx={{ ...tgTextFieldSx, flexBasis: 0, flexGrow: 1 }} 
+            />
+          </Box>
+
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button variant='outlined' onClick={onClose} sx={{ ml:1, mb: 1, minWidth: '120px' }}>
+              Close
+            </Button>
+            <Button variant='contained' type="submit" sx={{ ml:1, mb: 1, minWidth: '120px' }}>
+              Submit
+            </Button>
+          </Box>
+
+        </Box>
 
       </form>
 
@@ -177,56 +175,6 @@ function App() {
 }
 
 export default App
-
-/*
-
-  const tgTextFieldSx = { 
-    ml:1, 
-    mb: 2, 
-    input: { color: 'var(--tg-theme-text-color)' },
-    label: { color: 'var(--tg-theme-text-color)' },
-    div: { color: 'var(--tg-theme-text-color)' },
-  };
-
-  const handleCardNumberChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || /^[0-9\b]+$/.test(value)) {
-      setCardNumber(value);
-    }
-  };
-
-  const handleNameOnCardChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || /^[a-zA-Z\s]+$/.test(value)) {
-      setNameOnCard(value.toUpperCase());
-    }
-  };
-
-  const handleExpirationDateChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || /^[0-9/]+$/.test(value)) {
-      setExpiryDate(value);
-    }
-  };
-
-  const handleCvvChange = (e) => {
-    const value = e.target.value;
-    if (value === "" || /^[0-9\b]+$/.test(value)) {
-      setCvv(value);
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (cardNumber && expirationDate && cvv) {
-      console.log('Данные отправлены!');
-    } else {
-      console.log('Пожалуйста, заполните все поля');
-    }
-  };
-
-*/
-
 
 /*
 
