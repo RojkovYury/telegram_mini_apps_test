2401607c1c1d27903f18562696a60a651fdd5492
RojkovYury <rojkov.yury@gmail.com>
2023-10-24 04:26:11 +0300

    3

--- src/App.js
+++ src/App.js
@@ -81,7 +81,9 @@ function App() {
           <div style={{ width: '100%', paddingBottom: '16px' }}>
             <div style={{ paddingLeft: '10px', marginBottom: '2px', fontSize: '16px', color: 'var(--tg-theme-text-color)' }}>Card Number:</div>
             <div style={{ border: '2px solid var(--tg-theme-text-color)', borderRadius: '4px', display: 'flex' }}>
-              <div style={{ width: '24px', height: '24px', marginTop: '10px', marginBottom: '10px', marginLeft: '10px', marginRight: '10px' }}><CreditCardIcon/></div>
+              <div style={{ width: '24px', height: '24px', marginTop: '10px', marginBottom: '10px', marginLeft: '10px', marginRight: '10px' }}>
+                <CreditCardIcon sx={{ color: 'var(--tg-theme-text-color)' }}/>
+              </div>
               <input 
                 value={cardNumber}
                 onChange={handleCardNumberChange}
