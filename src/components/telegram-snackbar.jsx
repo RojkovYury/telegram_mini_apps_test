7a8329703379e0e3bc020bacdf42f9b21c7284cb
RojkovYury <rojkov.yury@gmail.com>
2023-10-27 00:26:57 +0300

    remove TransitionLeft

--- src/components/telegram-snackbar.jsx
+++ src/components/telegram-snackbar.jsx
@@ -4,9 +4,11 @@ import MuiAlert from '@mui/material/Alert';
 
 export default function TelegramSnackbar(props) {
 
-  function TransitionLeft(props) {
+  /*
+    function TransitionLeft(props) {
     return <Slide {...props} direction="right" />;
   }
+  */
 
   const Alert = forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -18,7 +20,7 @@ export default function TelegramSnackbar(props) {
       autoHideDuration={1500}
       onClose={props.onClose}
       anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      TransitionComponent={TransitionLeft}
+      // TransitionComponent={TransitionLeft}
     >
       <Alert onClose={props.onClose} severity="error" sx={{ width: '100%' }}>
         {props.message}
